a0ba100854b6cebbdabfd6817f97bbd82c48a172
Gülhan <48055578+Stay98@users.noreply.github.com>
2019-05-24 17:14:10 +0300

    Login/Register Olusturuldu. İyi Calismalar

---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyDOMeap_AevfduoynznbqQpkh200KLxUH0",
+    authDomain: "dessertrecip.firebaseapp.com",
+    databaseURL: "https://dessertrecip.firebaseio.com",
+    projectId: "dessertrecip",
+    storageBucket: "dessertrecip.appspot.com",
+    messagingSenderId: "992954427175",
+    appId: "1:992954427175:web:9ef096679fe83f4b"
+  }
+
+  export default config
